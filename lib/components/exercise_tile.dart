d1b9ef17a74e9a302cc0232dbeddeb7eed8db16c
MrE2000 <eliasymadani@gmail.com>
2025-01-02 16:40:00 +0300

    onCheckBoxChanged

---
+++ lib/components/exercise_tile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:gymapp/colors.dart';
+
+class ExerciseTile extends StatelessWidget {
+   ExerciseTile(
+      {super.key,
+      required this.onCheckBoxChanged,
+      required this.exerciseName,
+      required this.weights,
+      required this.reps,
+      required this.sets,
+      required this.isCompleted});
+  final String exerciseName;
+  final String weights;
+  final String reps;
+  final String sets;
+  final bool isCompleted;
+   void Function(bool?)? onCheckBoxChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      child: ListTile(
+          title: Text(exerciseName),
+          subtitle: Row(
+            children: [
+              // weight
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Chip(backgroundColor: Colors.grey[400], label: Text(weights)),
+              ),
+
+              // reps
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Chip(backgroundColor: Colors.grey[400], label: Text(reps)),
+              ),
+              // sets
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Chip(backgroundColor: Colors.grey[400], label: Text(sets)),
+              )
+            ],
+          ),
+          trailing: Checkbox(value: isCompleted, onChanged:(value) => onCheckBoxChanged!(value), ),
+          //,
+          ),
+    );
+  }
+}
--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gymapp/colors.dart';
+import 'package:gymapp/components/exercise_tile.dart';
+import 'package:gymapp/data/workout_data.dart';
+import 'package:provider/provider.dart';
 
 class WorkoutPage extends StatefulWidget {
   const WorkoutPage({super.key, required this.workoutName});
@@ -11,8 +15,56 @@ class WorkoutPage extends StatefulWidget {
 class _WorkoutPageState extends State<WorkoutPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text(widget.workoutName),),
+    return Consumer<WorkoutData>(
+      builder: (context, value, child) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.primaryColor,
+            title: Text(widget.workoutName,
+                style: const TextStyle(color: Colors.white)),
+          ),
+          body: ListView.builder(
+            itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
+            itemBuilder: (context, index) {
+              return ExerciseTile(
+                exerciseName: value
+                    .getRelevantWorkout(widget.workoutName)
+                    .exercises[index]
+                    .name,
+                weights: value
+                    .getRelevantWorkout(widget.workoutName)
+                    .exercises[index]
+                    .weight,
+                reps: value
+                    .getRelevantWorkout(widget.workoutName)
+                    .exercises[index]
+                    .reps,
+                sets: value
+                    .getRelevantWorkout(widget.workoutName)
+                    .exercises[index]
+                    .sets,
+                isCompleted: value
+                    .getRelevantWorkout(widget.workoutName)
+                    .exercises[index]
+                    .isCompleted,
+                onCheckBoxChanged: (val) => onCheckBoxChenged(
+                    context,
+                    widget.workoutName,
+                    value
+                        .getRelevantWorkout(widget.workoutName)
+                        .exercises[index]
+                        .name),
+              );
+            },
+          ),
+        );
+      },
     );
+  }
+
+  void onCheckBoxChenged(
+      BuildContext context, String workoutName, String exerciseName) {
+    Provider.of<WorkoutData>(context, listen: false)
+        .checkoffExercise(workoutName, exerciseName);
   }
 }
